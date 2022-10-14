7d563fe5cc1324034c10c6bbb819096e99d6ae69
LeandroDCM <ironsjc@hotmail.com>
2022-10-14 13:49:01 -0300

    line changes working

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
-
 const {checkToken} = require('./middleware/checkToken')
 const express = require('express');
 const routes = express.Router();
+
 // Models
 const {User} = require('./models/User')
 
@@ -13,15 +13,14 @@ routes.get('/', (req: any, res: any) => {
   res.status(200).json({msg: "Welcome"})
 })
 
-//Private Route 
-//function checkToken to check if token is authorized to access private route
-routes.get('/user/:id', checkToken, UserController.userIndex)
-
 // Register User Route
 routes.post('/auth/register', UserController.register)
 
 // Login User Route
-
 routes.post('/auth/login', UserController.login)
+
+//Private Route 
+//function checkToken to check if token is authorized to access private route
+routes.get('/user/:id', checkToken, UserController.userIndex)
 
 export { routes };
