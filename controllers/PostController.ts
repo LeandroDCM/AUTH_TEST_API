7459427e71a936eb8826f47787b9c429d7a50672
LeandroDCM <gamerleandro25@gmail.com>
2022-10-18 10:03:11 -0300

    added safety checks to userpost update route

--- controllers/PostController.ts
+++ controllers/PostController.ts
@@ -1,19 +1,24 @@
-const {Post} = require('../models/Post')
-const {User} = require('../models/User')
+const { Post } = require("../models/Post");
+const { User } = require("../models/User");
+const mongoose = require("mongoose");
 
 module.exports = {
-
   async post(req: any, res: any) {
     const { post } = req.body;
-    const id = req.params.id
-    const user = await User.findById(id, '-password')
+    const id = req.params.id;
+    const user = await User.findById(id, "-password");
+    if (!user) {
+      return res.json({
+        msg: "User not found",
+      });
+    }
     const newPost = new Post({
       name: user.name,
       user: user.id,
-      post
-    })
-    await newPost.save()
-    return res.json(newPost.post)
+      post,
+    });
+    await newPost.save();
+    return res.json(newPost.post);
   },
 
   async update(req: any, res: any) {
@@ -21,18 +26,57 @@ module.exports = {
     const id = req.params.id;
     const newPost = req.body;
 
-    const user = await User.findById(id, '-password');
+    //check for valid user id
+    const isValidUserId = mongoose.Types.ObjectId.isValid(id);
+    if (!isValidUserId)
+      return res.status(400).json({
+        msg: "Post id is not valid",
+      });
+
+    //check for valid post id
+    const isValidId = mongoose.Types.ObjectId.isValid(postid);
+    if (!isValidId)
+      return res.status(400).json({
+        msg: "Post id is not valid",
+      });
+
+    //find user and post
+    const user = await User.findById(id, "-password");
     const post = await Post.findById(postid);
 
-    const userId = user._id
-    const postUser = post.user
+    //check if user exists
+    if (user === null)
+      return res.status(400).json({
+        msg: "User id does not exists",
+      });
 
-    if(userId.toString() === postUser.toString()) {
-      await Post.findByIdAndUpdate(postid, newPost)
-      return res.json(newPost)
+    //check if post exists
+    if (post === null)
+      return res.status(400).json({
+        msg: "Post id does not exists",
+      });
+
+    const userId = user._id;
+    const postUser = post.user;
+
+    if (!user) {
+      return res.json({
+        msg: "User not found.",
+      });
+    }
+
+    if (!post) {
+      return res.json({
+        msg: "Post not found.",
+      });
+    }
+
+    if (userId.toString() === postUser.toString()) {
+      await Post.findByIdAndUpdate(postid, newPost);
+      return res.json(newPost);
     } else {
-      return res.json({Error: 'Cannot update another users post.'})
-    } 
+      return res.json({ Error: "Cannot update another users post." });
+    }
   },
 
   async delete(req: any, res: any) {
@@ -40,15 +84,15 @@ module.exports = {
       const postid = req.params.postid;
       const userid = req.params.id;
 
-      const thisPost = await Post.findById(postid)
+      const thisPost = await Post.findById(postid);
       if (userid.toString() !== thisPost.user.toString()) {
-        return res.send('Access denied. Cannot delete other users post.')
+        return res.send("Access denied. Cannot delete other users post.");
       }
 
       const post = await Post.findByIdAndDelete(postid);
 
       if (!post) res.status(404).send("No post found");
-      res.status(200).send('Post deleted successfully');
+      res.status(200).send("Post deleted successfully");
     } catch (error) {
       res.status(500).send(error);
     }
@@ -57,7 +101,7 @@ module.exports = {
   async index(req: any, res: any) {
     try {
       const posts = await Post.find({}, "name post -_id");
-      res.json(posts)
+      res.json(posts);
     } catch (error) {
       res.status(500).send(error);
     }
@@ -67,11 +111,12 @@ module.exports = {
     try {
       const id = req.params.id;
 
-      const posts = await Post.find({user: id.toString()}, "name post -_id");
-      res.json(posts)
+      const posts = await Post.find({ user: id.toString() }, "name post -_id");
+      res.json(posts);
     } catch (error) {
       res.status(500).send(error);
     }
   },
-}
+};
 
+export {};
