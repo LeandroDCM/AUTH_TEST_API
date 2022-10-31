cc902483f5afa8aab2897d522bc3e3fd4cea6913
LeandroDCM <gamerleandro25@gmail.com>
2022-10-31 09:23:56 -0300

    typed middleware checkToken

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import { NextFunction, Response, Request } from "express";
 
-function checkToken(req: any, res: any, next: any) {
+function checkToken(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   //catches the token from the header and makes it the right format
@@ -13,7 +14,12 @@ function checkToken(req: any, res: any, next: any) {
     const secret = process.env.SECRET as string;
 
     //verifies the token and no errors are thrown
-    const userInformation = jwt.verify(token, secret) as any;
+    const userInformation = jwt.verify(token, secret) as {
+      username: string;
+      session: string;
+      type: string;
+    };
+
     //passes the email to req.session to know which user is logged
     req.session = userInformation;
 
